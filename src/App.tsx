d38783a1b7438b86c3626860d1fac2e0e3180678
Aslan <aslankhatuev@Aslan-sin-MacBook-Air.local>
2024-11-01 11:39:09 +0100

    added footer functionallity

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import Header from "./components/header/Header"
 import Home from "./pages/Home"
 import Program from "./pages/Program"
 import Contact from "./pages/Contact"
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
     <>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/program" element={<Program />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import Logo from "../../assets/React.svg";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Link to="/">
+        <img src={Logo} alt="Logo for Javazone" />
+      </Link>
+      <nav>
+        <Link to="/privacy-policy">Privacy Policy</Link>
+        <Link to="/terms">Terms of Service</Link>
+        <Link to="/contact">Contact us</Link>
+      </nav>
+      <div className="social-links">
+        <a
+          href="https://www.facebook.com/javazoneconference"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a
+          href="https://x.com/search?q=javazone&src=typed_query"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/javazone/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
+      <p>&copy; {new Date().getFullYear()} Javazone.</p>
+    </footer>
+  );
+};
+
+export default Footer;
