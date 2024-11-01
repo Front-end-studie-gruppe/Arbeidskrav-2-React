80603874c4f1ee598cae2513e907f93eb111dd20
Simen Tveitane Skogen <simen.skogen@gokstadakademiet.no>
2024-11-01 11:46:06 +0100

    Added some styling variables. changed to module.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
+import "./index.module.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
