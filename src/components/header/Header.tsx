56fc204e03f7f9e0529f22f267c6bef67061ab45
Simen Tveitane Skogen <simen.skogen@gokstadakademiet.no>
2024-11-01 11:19:11 +0100

    Resturtured routes. Added header with some styling. Fix router-dom ts problems. Changed to module.css instead

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/React.svg";
+import headerStyle from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className={headerStyle.header}>
       <Link to="/">
         <img src={Logo} alt="Logo for Javazone" />
       </Link>
-      <nav>
+      <nav className={headerStyle.navLinks}>
         <Link to="/program">Program</Link>
-        <Link to="/contact">Contact us</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
     </header>
   );
