5c583a0f677b799df054b11870a3bd965cc79bd7
Simen Tveitane Skogen <simen.skogen@gokstadakademiet.no>
2024-11-04 11:05:41 +0100

    Changed global logo. Removed import conflict

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,13 +2,14 @@ import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/javazone-logo.jpg";
 import headerStyle from "./Header.module.css";
-import AuthSwitcher from "../auth/AuthSwitcher";
+import globalStyle from "../../index.module.css";
+import AuthSwitcher from "../Auth/AuthSwitcher";
 
 const Header = () => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [username, setUsername] = useState<string | null>(null);
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement | null>(null); 
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
 
   const toggleModal = () => {
     setModalOpen(!isModalOpen);
@@ -31,7 +32,10 @@ const Header = () => {
   // lukke dropdown når du klikker utenfor den
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
         setDropdownOpen(false);
       }
     };
@@ -46,7 +50,11 @@ const Header = () => {
     <>
       <header className={headerStyle.header}>
         <Link to="/">
-          <img src={Logo} alt="Logo for Javazone" />
+          <img
+            src={Logo}
+            className={globalStyle.logo}
+            alt="Logo for Javazone"
+          />
         </Link>
         <nav className={headerStyle.navContainer}>
           <ul className={headerStyle.navList}>
@@ -59,27 +67,32 @@ const Header = () => {
             <li className={headerStyle.listElement}>
               {username ? (
                 <>
-                  <p onClick={toggleDropdown} style={{ cursor: 'pointer' }}>{username}</p>
+                  <p onClick={toggleDropdown} style={{ cursor: "pointer" }}>
+                    {username}
+                  </p>
                   {isDropdownOpen && (
                     <div ref={dropdownRef} className={headerStyle.dropdown}>
-                      <Link to="/mypage" onClick={() => setDropdownOpen(false)}>My Page</Link>
+                      <Link to="/mypage" onClick={() => setDropdownOpen(false)}>
+                        My Page
+                      </Link>
                       <button onClick={handleLogout}>Log out</button>
                     </div>
                   )}
                 </>
               ) : (
-                <Link to="#" onClick={toggleModal}>My Account</Link>
+                <Link to="#" onClick={toggleModal}>
+                  My Account
+                </Link>
               )}
             </li>
           </ul>
         </nav>
       </header>
-      {isModalOpen && <AuthSwitcher onClose={toggleModal} onLogin={handleLogin} />}
+      {isModalOpen && (
+        <AuthSwitcher onClose={toggleModal} onLogin={handleLogin} />
+      )}
     </>
   );
 };
 
 export default Header;
-
-
-
