667422b0b1c5eb195430cab2150b0cebc22df8cc
Simen Tveitane Skogen <simen.skogen@gokstadakademiet.no>
2024-11-04 09:56:09 +0100

    Removed dummy data. was on wrong page

--- src/pages/homePage/Home.tsx
+++ src/pages/homePage/Home.tsx
@@ -5,34 +5,6 @@ const Home = () => {
     <>
       <section className={homeStyles.siteContainer}>
         <h1>Welcome to JavaZone</h1>
-        <div className={homeStyles.cardContainer}>
-          {/* HERE ADD CARD COMPONENT */}
-          <div className={homeStyles.card}>
-            <h2>Card Title 1</h2>
-            <p>This is a description for card 1.</p>
-            <button>Learn More</button>
-          </div>
-          <div className={homeStyles.card}>
-            <h2>Card Title 2</h2>
-            <p>This is a description for card 2.</p>
-            <button>Learn More</button>
-          </div>
-          <div className={homeStyles.card}>
-            <h2>Card Title 3</h2>
-            <p>This is a description for card 3.</p>
-            <button>Learn More</button>
-          </div>
-          <div className={homeStyles.card}>
-            <h2>Card Title 4</h2>
-            <p>This is a description for card 4.</p>
-            <button>Learn More</button>
-          </div>
-          <div className={homeStyles.card}>
-            <h2>Card Title 5</h2>
-            <p>This is a description for card 5.</p>
-            <button>Learn More</button>
-          </div>
-        </div>
       </section>
     </>
   );
