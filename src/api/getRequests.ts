83108f244e5562cdcb5b781183cd1b513d09365b
BendikBror <bendikbror@gmail.com>
2024-11-07 13:22:46 +0100

    lagt til cards og styling, fjernet mine get requests fra room og speakers.

--- src/api/getRequests.ts
+++ src/api/getRequests.ts
@@ -4,22 +4,21 @@ const getInfo = import.meta.env.VITE_API_KEY_SPEAKER;
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const getHeaders: HeadersInit = {
-  "Authorization": `Bearer ${getInfo}`,
+  Authorization: `Bearer ${getInfo}`,
 };
 
 export const getSpeakers = async (): Promise<Speaker[]> => {
-    const response = await fetch(`${BASE_URL}/speakers`, {
-      method: "GET",
-      headers: getHeaders,
-    });
-  
-    if (!response.ok) {
-      console.error("Failed to fetch speakers");
-      
-      throw new Error("Could not fetch speakers");
-    }
-  
-    const data = await response.json();
-    return data.items; 
-  };
-  
+  const response = await fetch(`${BASE_URL}/speakers`, {
+    method: "GET",
+    headers: getHeaders,
+  });
+
+  if (!response.ok) {
+    console.error("Failed to fetch speakers");
+
+    throw new Error("Could not fetch speakers");
+  }
+
+  const data = await response.json();
+  return data.items;
+};
--- src/pages/program/Program.tsx
+++ src/pages/program/Program.tsx
@@ -1,43 +1,19 @@
 import Programheader from "../../components/programHeader/Programheader";
 import programStyles from "./Program.module.css";
+import ProgramCards from "../../components/cards/Cards";
 
 const Program = () => {
   return (
     <>
-    <header>
-      <Programheader />
-    </header>
-     <section className={programStyles.siteContainer}>
-      <h1>Our program</h1>
-      <div className={programStyles.cardContainer}>
-        {/* HERE ADD CARD COMPONENT, UNDER IS DUMMY DATA*/}
-        <div className={programStyles.card}>
-          <h2>Card Title 1</h2>
-          <p>This is a description for card 1.</p>
-          <button>Learn More</button>
+      <header>
+        <Programheader />
+      </header>
+      <section className={programStyles.siteContainer}>
+        <h1>Our program</h1>
+        <div className={programStyles.cardContainer}>
+          <ProgramCards />
         </div>
-        <div className={programStyles.card}>
-          <h2>Card Title 2</h2>
-          <p>This is a description for card 2.</p>
-          <button>Learn More</button>
-        </div>
-        <div className={programStyles.card}>
-          <h2>Card Title 3</h2>
-          <p>This is a description for card 3.</p>
-          <button>Learn More</button>
-        </div>
-        <div className={programStyles.card}>
-          <h2>Card Title 4</h2>
-          <p>This is a description for card 4.</p>
-          <button>Learn More</button>
-        </div>
-        <div className={programStyles.card}>
-          <h2>Card Title 5</h2>
-          <p>This is a description for card 5.</p>
-          <button>Learn More</button>
-        </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
