fb3260439805d1d26219260e7d9f23aae1ae2aa0
Aslan Khatuev <aslan.khatuev@outlook.com>
2024-11-07 19:29:31 +0100

    Homepage og styling er gjort

--- src/pages/homePage/Home.tsx
+++ src/pages/homePage/Home.tsx
@@ -1,12 +1,19 @@
-import homeStyles from "./Home.module.css";
+import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <>
-      <section className={homeStyles.siteContainer}>
-        <h1>Welcome to JavaZone</h1>
-      </section>
-    </>
+    <div className={styles["home-container"]}>
+      <h1>Welcome to JavaZone</h1>
+      <p>
+        September 4th-5th 2024 in Oslo Spektrum, Norway Conference: September
+        4th-5th Workshops: September 3rd Workshop registration opens on August
+        19th
+      </p>
+      <button onClick={() => (window.location.href = "/program")}>
+        Explore Program
+      </button>
+    </div>
   );
 };
 
