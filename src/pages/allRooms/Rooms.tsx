1deccfa96390dbdaac9438d2611b76781d35f034
Adam Maltsagov <adam.maltsagov@gokstadakademiet.no>
2024-11-08 00:33:27 +0100

    Styling

--- src/pages/allRooms/Rooms.tsx
+++ src/pages/allRooms/Rooms.tsx
@@ -2,6 +2,8 @@ import Programheader from "../../components/programHeader/Programheader"
 import { getRooms } from "../../api/getRequests"
 import { useEffect, useState } from "react";
 import { room } from "../../types/types"
+import stylethis from "../../style/globalStyle.module.css"
+
 
 
 const Rooms = () => {
@@ -11,9 +13,9 @@ const Rooms = () => {
 
   useEffect(() => {
 
-    const fetchRooms  = async () => {
+    const fetchRooms = async () => {
       try {
-        const dataRooms = await getRooms ();
+        const dataRooms = await getRooms();
         console.log("Fetched rooms:", dataRooms);
         setRooms(dataRooms);
       } catch (error) {
@@ -29,34 +31,30 @@ const Rooms = () => {
     return <div>Loading Rooms...</div>;
   }
 
-
-  
-  
-
   return (
-    <>
+    <div className={stylethis.all}>
       <header>
         <Programheader />
       </header>
-      <div>
-        <h2>Rooms</h2>
+      <h2 className={stylethis.hTwo}>Rooms</h2>
+      <div className={stylethis.flexCenter}>
+        <section>
+          {rooms.length === 0 ? (
+            <p>There are no rooms available</p>
+          ) : (
+            <ul>
+              {rooms.map((room) => (
+                <li
+                  key={room._uuid}>
+                  <h3>{room.name}</h3>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
-      <section>
-        {rooms.length === 0 ? (
-          <p>There are no rooms available</p>
-        ) : (
-          <ul>
-            {rooms.map((room) => (
-              <li 
-              key={room._uuid}>
-                <h3>{room.name}</h3>
-              </li>
-            ))}
-          </ul>
-        )}
-      </section>
-       </>
-     );
-   };
+    </div>
+  );
+};
 
 export default Rooms
