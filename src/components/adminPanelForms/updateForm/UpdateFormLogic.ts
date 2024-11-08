4a8fb45f309fcb2b21115b36d43d027868612600
Simen Tveitane Skogen <simen.skogen@gokstadakademiet.no>
2024-11-08 10:22:23 +0100

    Added filtering after deleting to reset data in option list

--- src/components/adminPanelForms/updateForm/UpdateFormLogic.ts
+++ src/components/adminPanelForms/updateForm/UpdateFormLogic.ts
@@ -71,10 +71,6 @@ const useAdminLogic = (
               endTime: selectedTalk.endTime,
             });
           }
-        } else if (selectedName === "speakerId") {
-          setTalkData((prev) => ({ ...prev, speakerId: Number(selectedValue) }));
-        } else if (selectedName === "roomId") {
-          setTalkData((prev) => ({ ...prev, roomId: Number(selectedValue) }));
         }
         break;
       case "speakers":
@@ -159,6 +155,7 @@ const useAdminLogic = (
       switch (formType) {
         case "talks":
           await deleteTalk(talkData._uuid);
+          setTalkOptions((prevOptions) => prevOptions.filter((talk) => talk._uuid !== talkData._uuid));
           setTalkData({
             _uuid: "",
             title: "",
