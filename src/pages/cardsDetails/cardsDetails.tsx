87db724b4370f6d7bb94b36bde1b7c1b0ce8e177
Simen Tveitane Skogen <simen.skogen@gokstadakademiet.no>
2024-11-08 11:39:34 +0100

    Reused styling for detail page

--- src/pages/cardsDetails/cardsDetails.tsx
+++ src/pages/cardsDetails/cardsDetails.tsx
@@ -2,6 +2,7 @@ import Programheader from "../../components/programHeader/Programheader";
 import { getRooms, getSpeakers, getTalks } from "../../api/getRequests";
 import { useEffect, useState } from "react";
 import { room, Speaker, talk } from "../../types/types";
+import cardStyle from "../homePage/Home.module.css";
 
 const ProgramOverview = () => {
   const [rooms, setRooms] = useState<room[]>([]);
@@ -38,61 +39,63 @@ const ProgramOverview = () => {
 
   return (
     <>
-      <header>
-        <Programheader />
-      </header>
-      <div>
-        <h2>Program Overview</h2>
-      </div>
+      <section className={cardStyle.homeContainer}>
+        <header>
+          <Programheader />
+        </header>
+        <div>
+          <h2>Program Overview</h2>
+        </div>
 
-      <section>
-        <h3>Rooms</h3>
-        {rooms.length === 0 ? (
-          <p>There are no rooms available</p>
-        ) : (
-          <ul>
-            {rooms.map((room) => (
-              <li key={room._uuid}>
-                <h4>{room.name}</h4>
-              </li>
-            ))}
-          </ul>
-        )}
-      </section>
+        <section>
+          <h3>Rooms</h3>
+          {rooms.length === 0 ? (
+            <p>There are no rooms available</p>
+          ) : (
+            <ul>
+              {rooms.map((room) => (
+                <li key={room._uuid}>
+                  <h4>{room.name}</h4>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
 
-      <section>
-        <h3>Speakers</h3>
-        {speakers.length === 0 ? (
-          <p>There are no speakers available</p>
-        ) : (
-          <ul>
-            {speakers.map((speaker) => (
-              <li key={speaker._uuid}>
-                <h4>{speaker.name}</h4>
-                <p>Bio: {speaker.bio}</p>
-              </li>
-            ))}
-          </ul>
-        )}
-      </section>
+        <section>
+          <h3>Speakers</h3>
+          {speakers.length === 0 ? (
+            <p>There are no speakers available</p>
+          ) : (
+            <ul>
+              {speakers.map((speaker) => (
+                <li key={speaker._uuid}>
+                  <h4>{speaker.name}</h4>
+                  <p>Bio: {speaker.bio}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
 
-      <section>
-        <h3>Talks</h3>
-        {talks.length === 0 ? (
-          <p>There are no talks available</p>
-        ) : (
-          <ul>
-            {talks.map((talk) => (
-              <li key={talk._uuid}>
-                <h4>{talk.title}</h4>
-                <p>Speaker: {talk.speakerId}</p>
-                <p>Room: {talk.roomId}</p>
-                <p>Start Time: {talk.startTime}</p>
-                <p>End Time: {talk.endTime}</p>
-              </li>
-            ))}
-          </ul>
-        )}
+        <section>
+          <h3>Talks</h3>
+          {talks.length === 0 ? (
+            <p>There are no talks available</p>
+          ) : (
+            <ul>
+              {talks.map((talk) => (
+                <li key={talk._uuid}>
+                  <h4>{talk.title}</h4>
+                  <p>Speaker: {talk.speakerId}</p>
+                  <p>Room: {talk.roomId}</p>
+                  <p>Start Time: {talk.startTime}</p>
+                  <p>End Time: {talk.endTime}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
       </section>
     </>
   );
